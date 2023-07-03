3fc4779157df74fa32fd06d1b046d6dc2591e4b0
Marc Klingen <git@marcklingen.com>
2023-07-03 16:56:20 +0200

    Show successful vote as state

--- components/chat-message-actions.tsx
+++ components/chat-message-actions.tsx
@@ -6,10 +6,17 @@ import { Button } from '@/components/ui/button'
 import { IconCheck, IconCopy } from '@/components/ui/icons'
 import { useCopyToClipboard } from '@/lib/hooks/use-copy-to-clipboard'
 import { cn } from '@/lib/utils'
-import { ThumbsUp, ThumbsDown } from 'lucide-react'
+import {
+  FaRegThumbsUp,
+  FaRegThumbsDown,
+  FaThumbsDown,
+  FaThumbsUp
+} from 'react-icons/fa'
 
 import { LangfuseWeb } from 'langfuse'
 import { useState } from 'react'
+import clsx from 'clsx'
+import { toast } from 'react-hot-toast'
 
 interface ChatMessageActionsProps extends React.ComponentProps<'div'> {
   message: Message
@@ -20,6 +27,8 @@ const langfuse = new LangfuseWeb({
   publicKey: process.env.NEXT_PUBLIC_LANGFUSE_PUBLIC_KEY ?? ''
 })
 
+type Vote = 'up' | 'down'
+
 export function ChatMessageActions({
   message,
   chatId,
@@ -28,13 +37,14 @@ export function ChatMessageActions({
 }: ChatMessageActionsProps) {
   const { isCopied, copyToClipboard } = useCopyToClipboard({ timeout: 2000 })
   const [isVoting, setIsVoting] = useState(false)
+  const [vote, setVote] = useState<Vote | null>(null)
 
   const onCopy = () => {
     if (isCopied) return
     copyToClipboard(message.content)
   }
 
-  const handleFeedback = (score: number) => {
+  const handleFeedback = (vote: Vote) => {
     if (isVoting || !chatId) return
     setIsVoting(true)
     langfuse
@@ -42,9 +52,14 @@ export function ChatMessageActions({
         traceId: `chat:${chatId}`,
         traceIdType: 'EXTERNAL',
         name: 'user-feedback',
-        value: score
+        value: vote === 'up' ? 1 : -1
       })
       .then(res => {
+        setIsVoting(false)
+        setVote(vote)
+      })
+      .catch(err => {
+        toast.error('Something went wrong')
         setIsVoting(false)
       })
   }
@@ -52,7 +67,7 @@ export function ChatMessageActions({
   return (
     <div
       className={cn(
-        'flex flex-col items-center justify-end transition-opacity group-hover:opacity-100 md:absolute md:-right-10 md:opacity-0',
+        'flex flex-col items-center justify-end transition-opacity md:absolute md:-right-10',
         message.role === 'assistant' ? 'md:top-[-36px]' : 'md:-top-2',
         className
       )}
@@ -62,14 +77,28 @@ export function ChatMessageActions({
         <Button
           variant="ghost"
           size="iconXs"
-          onClick={() => handleFeedback(1)}
+          className={clsx(
+            vote === 'up'
+              ? 'md:opacity-100'
+              : 'group-hover:opacity-100 md:opacity-0'
+          )}
+          onClick={() => handleFeedback('up')}
           disabled={isVoting}
         >
-          <ThumbsUp className="h-3 w-3" />
+          {vote === 'up' ? (
+            <FaThumbsUp className="h-3 w-3" />
+          ) : (
+            <FaRegThumbsUp className="h-3 w-3" />
+          )}
           <span className="sr-only">Upvote</span>
         </Button>
       ) : null}
-      <Button variant="ghost" size="iconXs" onClick={onCopy}>
+      <Button
+        variant="ghost"
+        size="iconXs"
+        onClick={onCopy}
+        className="group-hover:opacity-100 md:opacity-0"
+      >
         {isCopied ? <IconCheck /> : <IconCopy />}
         <span className="sr-only">Copy message</span>
       </Button>
@@ -77,10 +106,19 @@ export function ChatMessageActions({
         <Button
           variant="ghost"
           size="iconXs"
-          onClick={() => handleFeedback(0)}
+          className={clsx(
+            vote === 'down'
+              ? 'md:opacity-100'
+              : 'group-hover:opacity-100 md:opacity-0'
+          )}
+          onClick={() => handleFeedback('down')}
           disabled={isVoting}
         >
-          <ThumbsDown className="h-3 w-3" />
+          {vote === 'down' ? (
+            <FaThumbsDown className="h-3 w-3" />
+          ) : (
+            <FaRegThumbsDown className="h-3 w-3" />
+          )}
           <span className="sr-only">Downvote</span>
         </Button>
       ) : null}
